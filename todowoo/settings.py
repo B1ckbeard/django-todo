686ec79fc14f76042f9cda70ddd718fea722f9d9
Black_Beard <evs.diman@yandex.ru>
2021-11-19 15:37:49 +0300

    19

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static/')
-STATIC_ROOT =  os.path.join(BASE_DIR, 'static').
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
