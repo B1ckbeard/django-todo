46b50d680e1a773e61a52f0e3697583acc5ae1c1
Black_Beard <evs.diman@yandex.ru>
2021-11-19 11:36:53 +0300

    5 comm

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-li((gg4!$nfxhdh9!+m+^6z3zy_9-+^$$8358@&c2--!%w&z3#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
