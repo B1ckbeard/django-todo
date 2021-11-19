7577a7359b48b64f8ee989378e63df2f6bbaad01
Black_Beard <evs.diman@yandex.ru>
2021-11-19 12:12:05 +0300

    9 comm

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -134,13 +134,15 @@ STATIC_URL = 'todo/static/'
 LOGIN_URL = '/login'
 
 # add for heroku
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+if DEBUG:
+   STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+   ]
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
-# Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
