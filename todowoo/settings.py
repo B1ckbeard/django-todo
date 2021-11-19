ce052e846f72545e4c400c38b796c38bab70c9c4
Black_Beard <evs.diman@yandex.ru>
2021-11-19 15:35:06 +0300

    18 comm

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(
-    os.path.dirname(BASE_DIR), 'static/')
+#STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static/')
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static').
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
