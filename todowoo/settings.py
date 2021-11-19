82e092568dedd0fa2a9e9eefcd2d758fdd9c3be4
Black_Beard <evs.diman@yandex.ru>
2021-11-19 12:24:05 +0300

    10 comm

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -12,14 +12,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-#for heroku
-import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 #for heroku
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
