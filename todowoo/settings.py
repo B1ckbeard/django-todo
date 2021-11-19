0c3beb3400f2e4b7e23a63cf4f0ebc11d0b91448
Black_Beard <evs.diman@yandex.ru>
2021-11-19 11:14:08 +0300

    4 comm

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-li((gg4!$nfxhdh9!+m+^6z3zy_9-+^$$8358@&c2--!%w&z3#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
